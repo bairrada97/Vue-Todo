0517c926c4fefb8702be2ab1ce00efc7c4dacd6d
Unknown <bairrada1997@gmail.com>
2018-12-06 14:08:46 +0000

    ADD: files

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button type="button" name="button">{{ label }}</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: ['label']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  button{
+    cursor: pointer;
+    border-radius: 5%;
+    width: 100px;
+    height: 50px;
+    border: 0;
+    margin: 20px;
+  }
+</style>
---
+++ src/components/CompletedList.vue
@@ -0,0 +1,18 @@
+<template>
+  <li>{{ completed }}</li>
+</template>
+
+<script>
+export default {
+  name: 'CompletedList',
+  props: ['completed']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  button{
+    border: 2px solid green;
+    padding: 30px;
+  }
+</style>
